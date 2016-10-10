52fccad3915f0262698d9830296ca60ce230201a
meetshah1995 <meetshah1995@gmail.com>
2016-10-10 10:09:07 +0530

    Add base CNN network

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,13 +1,15 @@
 import numpy as np
 import librosa as lb
 
+from scipy import misc
+
 Fs         = 12000
 N_FFT      = 512
 N_MELS     = 96
 N_OVERLAP  = 256
 DURA       = 29.12
 
-def log_scale_melspectrogram(path):
+def log_scale_melspectrogram(path, plot=False):
     signal, sr = lb.load(path, sr=Fs)
     n_sample = signal.shape[0]
     n_sample_fit = int(DURA*Fs)
@@ -19,4 +21,8 @@ def log_scale_melspectrogram(path):
 
     melspect = lb.logamplitude(lb.feature.melspectrogram(y=signal, sr=Fs, hop_length=N_OVERLAP, n_fft=N_FFT, n_mels=N_MELS)**2, ref_power=1.0)
     melspect = melspect[np.newaxis, np.newaxis, :]
+    
+    if plot:
+        misc.imshow(melspect.reshape((melspect.shape[2],melspect.shape[3])))
+
     return melspect
